a57d728c9e4d6ee7014e9b4a644748f79bbae5a6
Trend20 <enockomondi305@gmail.com>
2022-12-01 06:55:28 +0300

    coderbyte solutions

---
+++ coderbyte/AphabetSoup.js
@@ -0,0 +1,23 @@
+/*
+QUESTION:
+  Have the function AlphabetSoup(str) take the str string parameter being passed 
+  and return the string with the letters in alphabetical order (ie. hello becomes ehllo). 
+  Assume numbers and punctuation symbols will not be included in the string.
+*/
+
+// TESTS SORTING 
+
+const AlphabetSoup= (str) =>{
+  // first make the string to be an array
+  const strArr = str.split('');
+  // sort the array
+  const sortedStrArr = strArr.sort();
+  // join the resulted array to string again
+  const result = sortedStrArr.join('');
+  return result;
+
+  // can also be done in a single line of code
+  return str.split("").sort().join("");
+} 
+
+AlphabetSoup(readline());
