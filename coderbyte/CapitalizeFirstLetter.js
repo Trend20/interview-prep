c80e9ae06f60f788ca78772482e6879511db109e
Trend20 <enockomondi305@gmail.com>
2022-12-01 15:14:18 +0300

    capitalize

--- coderbyte/CapitalizeFirstLetter.js
+++ coderbyte/CapitalizeFirstLetter.js
@@ -9,3 +9,27 @@ const CapitalizeFirstLetter = (str) =>{
       return (firstCharLetter + consequesntLetter).join(" ");
    }
 }
+
+/*
+QUESITON:
+Have the function LetterCapitalize(str) take the str parameter being passed 
+and capitalize the first letter of each word. Words will be separated by only one space. 
+*/
+
+function LetterCapitalize(str){
+
+   let genArr = str.split(" ");
+
+   // loop the array
+   for(let i = 0; i<genArr.length; i++){
+
+      let word = genArr[i].split("");
+      word[0] = word[0].toUpperCase();
+      genArr[i] = word.join("")
+   }
+
+   return genArr.join(" ");
+}
+
+// keep this function call here
+LetterCapitalize(readline());
--- coderbyte/FirstFactorial.js
+++ coderbyte/FirstFactorial.js
@@ -5,3 +5,13 @@ return the factorial of it. For example: if num = 4, then your program should
 return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer. 
 */
 
+function firstFactorial(num){
+  let factorial = 1;
+
+  // loop through num
+  for(let i = 1; i <= num; i++){
+    factorial *= i;
+    console.log(factorial);
+  }
+  return factorial;
+}
